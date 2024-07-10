e0e8f09756f58190537575c53ec71b8ade775e52
kerian.allaire <kerian.allaire@epita.fr>
2024-07-10 15:34:10 +0200

    fix(merge): first functioning version

--- src/stream_reader.py
+++ src/stream_reader.py
@@ -15,10 +15,15 @@ HEIGHT = 640
 fossil1 = {"name": "human_bone", "path": "objects/bone.png", "scale_factor": 0.25}
 fossils = load_objects_texture([fossil1] * 10)
 BG_IMG, FG_IMG, Z_IMG = create_textures(fossils, sdbx_width=WIDTH, sdbx_height=HEIGHT)
+BG_IMG, FG_IMG, Z_IMG = (
+    np.transpose(BG_IMG, (1, 0, 2)),
+    np.transpose(FG_IMG, (1, 0, 2)),
+    np.transpose(Z_IMG),
+)
 
 IID_RGB = 0
 IID_DPT = 0
-MAX_DEPTH = 1000  # Max depth in mm (adapt with the real depth)
+MAX_DEPTH = 3000  # Max depth in mm (adapt with the real depth)
 running = True
 
 
@@ -30,28 +35,27 @@ def sighandler(signal, frame):
 
 
 def depth_callback(dev, data, timestamp):
-    print("depth_callback")
     imsave("test_depth.png", data)
-    print(data.shape)
-    print(data)
+    imsave("FG.png", FG_IMG)
+    imsave("BG.png", BG_IMG)
     # normalize data
     depth_img = np.minimum(data, MAX_DEPTH) / MAX_DEPTH
-
     mask_z = Z_IMG <= depth_img
-    depth_img[mask_z] = FG_IMG[mask_z]
-    depth_img[~mask_z] = BG_IMG[~mask_z]
+    new_image = np.zeros((WIDTH, HEIGHT, 3), dtype=np.uint8)
+    new_image[mask_z] = FG_IMG[mask_z]
+    new_image[~mask_z] = BG_IMG[~mask_z]
 
-    cv2.imshow("Depth Stream", depth_img)
+    cv2.imshow("Depth Stream", new_image)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         global running
         running = False
 
 
 def rgb_callback(dev, data, timestamp):
-    print("rgb_callback")
-    imsave("test.png", data)
-    print(data.shape)
-    print(data)
+    # print("rgb_callback")
+    # imsave("test.png", data)
+    # print(data.shape)
+    # print(data)
 
     # Convertir le format d'image de RGB Ã BGR pour OpenCV
     # rgb_img = cv2.cvtColor(data, cv2.COLOR_RGB2BGR)
@@ -59,6 +63,7 @@ def rgb_callback(dev, data, timestamp):
     # if cv2.waitKey(1) & 0xFF == ord('q'):
     #     global running
     #     running = False
+    return
 
 
 def main():
