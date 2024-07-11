15cc3d2e206d04f7bb7ff409cc1c4fd4314c9029
ValentineTcheou <valentine.tcheou@epita.fr>
2024-07-11 13:39:39 +0200

    feat: clean frontend

--- src/frontend/front.py
+++ src/frontend/front.py
@@ -36,69 +36,87 @@ app.layout = html.Div(
         html.H4(
             id='header',
             children='FossilHunt',
-            style={'font-size': '45px', 'font-weight': 'bold', 'margin-bottom': '30px', 'position': 'absolute', 'top': '100px', 'left': '50%', 'transform': 'translateX(-50%)'}
+            style={
+                'font-size': '45px',
+                'font-weight': 'bold',
+                'margin-bottom': '30px',
+                'position': 'absolute',
+                'top': '100px',
+                'left': '50%',
+                'transform': 'translateX(-50%)'
+            }
         ),
         html.Div(
             id='content-start',
             children=[
                 html.Div(
-                    dcc.Dropdown(['test', 'customize'], 'test', id='model-dropdown', clearable=False),
-                    style = {'margin-bottom': '20px'}
+                    dcc.Dropdown(
+                        ['test', 'customize'],
+                        'test',
+                        id='model-dropdown',
+                        clearable=False
                     ),
-                    html.Div(
-            id='content-customize',
-            style = {'display' : 'none'}, 
-            #style={'position': 'fixed', 'top': '50%', 'left': '50%', 'transform': 'translate(-50%, -50%)', 'background': 'white', 'padding': '20px', 'box-shadow': '0px 0px 10px rgba(0,0,0,0.1)'},
-            children=[
+                    style = {'margin-bottom': '20px'}
+                ),
                 html.Div(
+                    id='content-customize',
+                    style = {'display' : 'none'},
+                    #style={'position': 'fixed', 'top': '50%', 'left': '50%', 'transform': 'translate(-50%, -50%)', 'background': 'white', 'padding': '20px', 'box-shadow': '0px 0px 10px rgba(0,0,0,0.1)'},
                     children=[
-                        html.H4('Customize Bones'),
-                        dcc.Dropdown(
-                            id='bone-name-dropdown',
-                            options=[{'label': bone['name'], 'value': bone['name']} for bone in bones_data['os']],
-                            placeholder='Select a bone'
-                        ),
-                        dcc.Input(
-                            id='orientation',
-                            type='number',
-                            min=0,
-                            max=360,
-                            step=5,
-                            placeholder='Orientation',
-                            value=45
-                        ),
-                        dcc.Input(
-                            id='bone-scale',
-                            type='number',
-                            placeholder='Scale factor',
-                            min=0,
-                            max=2,
-                            step=0.05,
-                            value=0.5
-                        ),
-                        html.Button(
-                            'Add Bone',
-                            id='add-bone-button',
-                            n_clicks=0
-                        ),
-                        html.Div(id='bone-list', children=[]),
-                        html.Button(
-                            'Submit',
-                            id='submit-bone-list',
-                            n_clicks=0
-                        ),
-                        html.Div(id='dummy-output')
+                        html.Div(
+                            children=[
+                                html.H4('Customize Bones'),
+                                dcc.Dropdown(
+                                    id='bone-name-dropdown',
+                                    options=[
+                                        {
+                                            'label': bone['name'],
+                                            'value': bone['name']
+                                         } for bone in bones_data['os']
+                                    ],
+                                    placeholder='Select a bone'
+                                ),
+                                dcc.Input(
+                                    id='orientation',
+                                    type='number',
+                                    min=0,
+                                    max=360,
+                                    step=5,
+                                    placeholder='Orientation',
+                                    value=45
+                                ),
+                                dcc.Input(
+                                    id='bone-scale',
+                                    type='number',
+                                    placeholder='Scale factor',
+                                    min=0,
+                                    max=2,
+                                    step=0.05,
+                                    value=0.5
+                                ),
+                                html.Button(
+                                    'Add Bone',
+                                    id='add-bone-button',
+                                    n_clicks=0
+                                ),
+                                html.Div(id='bone-list', children=[]),
+                                html.Button(
+                                    'Submit',
+                                    id='submit-bone-list',
+                                    n_clicks=0
+                                ),
+                                html.Div(id='dummy-output')
+                            ]
+                        )
                     ]
-                )
-            ]
-        ),
+                ),
                 html.Button(
                     'Commemcer le jeu',
                     id='start-button',
                     n_clicks =0
-                    )
-                ]
-            ),
+                )
+            ]
+        ),
         html.Div(
             id='content-game',
             children=[
@@ -107,26 +125,31 @@ app.layout = html.Div(
                     interval=1000,  # in milliseconds
                     n_intervals=0
                 ),
-                html.Div(children=[], id='model-name', style = {'margin-bottom': '5px'}),
-                html.Div(children=[], id='nb-bone-found', style = {'margin-bottom': '5px'}),
-                html.Div(children=[], id='time-elapsed', style = {'margin-bottom': '20px'}),
+                html.Div(
+                    children=[],
+                    id='model-name',
+                    style = {'margin-bottom': '5px'}
+                ),
+                html.Div(
+                    children=[],
+                    id='time-elapsed',
+                    style = {'margin-bottom': '20px'}
+                ),
                 html.Button(
                     'Finir la partie',
                     id='quit-button'
                 ),
-                html.Div(children=0, id='timer', style={'display': 'none'}),
-                html.Div(children=0, id='bone-count', style={'display': 'none'}),
-                html.Button(
-                    id='bone-button',
-                    n_clicks=0,
+                html.Div(
+                    children=0,
+                    id='timer',
                     style={'display': 'none'}
                 ),
-                ],
+            ],
             style = {'display': 'none'}
-            ),
-            
-     
-        ],
+        ),
+
+
+    ],
     style={
         'backgroundImage': 'url(/assets/background.png)',
         'backgroundSize': 'cover',
@@ -201,23 +224,21 @@ def submit_bone_list(n_clicks, bones):
         print(json.dumps(bone_data))
         return no_update
     return no_update
-        
+
 
 @callback(
     Output('content-start', component_property='style', allow_duplicate=True),
     Output('content-game', component_property='style', allow_duplicate=True),
     Output('model-name', 'children'),
-    Output('nb-bone-found', 'children', allow_duplicate=True),
     Output('time-elapsed', 'children', allow_duplicate=True),
     Output('timer', 'children', allow_duplicate=True),
-    Output('bone-count', 'children', allow_duplicate=True),
     Input('start-button',  'n_clicks'),
     State('model-dropdown', 'value'),
     prevent_initial_call=True
 )
 def start_game(n_clicks, model):
     if n_clicks > 0:
-        return {'display': 'none'}, {'display': 'block'}, f'Modèle : {model}', 'Nombre d\'os trouvé(s) : 0', 'Temps écoulé : 0:00:00', 0, 0
+        return {'display': 'none'}, {'display': 'block'}, f'Modèle : {model}', 'Temps écoulé : 0:00:00', 0
     return no_update
 
 
@@ -243,19 +264,6 @@ def update_time_elapsed(n_intervals, timer):
 def quit_game(n_clicks):
     if n_clicks > 0:
         return {'display': 'none'}, {'display': 'block'}
-    return no_update
-
-@callback(
-    Output('nb-bone-found', 'children', allow_duplicate=True),
-    Output('bone-count', 'children', allow_duplicate=True),
-    Input('bone-button', 'n_clicks'),
-    State('bone-count', 'children'),
-    prevent_initial_call=True
-)
-def update_bone_count(n_clicks, bone_count):
-    if n_clicks > 0:
-        bone_count += 1
-        return f'Nombre d\'os trouvé(s) : {bone_count}', bone_count
     return no_update
 
 if __name__ == '__main__':
